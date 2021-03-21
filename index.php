13fd63342687ff3f28de612a971e69bb1d5ff6c8
P-ROGER06 <proger06@gmail.com>
2021-03-21 14:34:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+$characters = [
+
+    "Philippe" => [
+
+        "city" => "Bordeaux",
+        "weapon" => "epic sword"
+
+    ],
+
+    "Negan" => [
+
+        "city" => "The Sanctuary",
+
+        "weapon" => "Lucille"
+
+    ],
+
+    "Daryl" => [
+
+        "city" => "Alexandria",
+
+        "weapon" => "crossbow"
+
+    ],
+
+    "Ezekiel" => [
+
+        "city" => "The Kingdom",
+
+        "weapon" => "Shiva"
+
+    ]
+
+];
+
+var_dump($characters);
